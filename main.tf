05b64e8648a795e116db4ac79756d686039266d6
toketunji <toketunji@gmail.com>
2020-05-31 09:34:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,6 @@
 terraform {
- /* required_version = "~>0.12.23"
-
-  backend "s3" {
-    bucket = "teal-tf-envs"
-    key = "tf_state"
-    region = "eu0-west-1"
-    dynamodb_table = "teal-tf-envs"
-    profile = "teal-mgmt"
-  }*/
   required_providers {
     aws = "~> 2.8"
-  }
-}
-
-provider "aws" {
-  region = "eu-west-1"
-  profile = "nonprod"
-
-  assume_role {
-    role_arn = "arn:aws:iam::009011612032:role/Terra-Role"
   }
 }
 
